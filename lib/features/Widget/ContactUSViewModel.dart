6d707ab81c7f2bd665fa8f0f794f2f7fcd335066
zikryfebriawan <zikry@dihardjasoftware.com>
2020-12-08 13:18:00 +0700

    nih kursa

---
+++ lib/features/Widget/ContactUSViewModel.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ListContactUs {
+  String title;
+  Color color;
+  IconData icon;
+  bool icon2;
+
+  ListContactUs(this.title, this.icon, this.color, this.icon2);
+}
