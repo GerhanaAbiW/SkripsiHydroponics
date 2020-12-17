4e8826b6e03467486fccec0a5c72948134c7d7f8
Gerhana Abi W <gerhana@dihardjasoftware.com>
2020-12-17 15:49:56 +0700

    aduh

--- lib/core/Providers/OrderProvider.dart
+++ lib/core/Providers/OrderProvider.dart
@@ -63,20 +63,20 @@ class OrderProvider with ChangeNotifier {
     for (int i = 0; i < _buyers.length; i++) {
       _revenue += _buyers[i].totalPrice;
       _sales += _buyers[i].totalQuantityProduct;
-      if (_buyers[i].cart != null || buyers[i].cart != []) {
-        buyers[i].cart = cart;
-        if (cart[i].productCategory == "Bibit") {
-          _bibit = _bibit + 1;
-        } else if (cart[i].productCategory == "Pupuk") {
-          _pupuk = _pupuk + 1;
-        } else if (cart[i].productCategory == "Obat") {
-          _obat = _obat + 1;
-        } else if (cart[i].productCategory == "Alat") {
-          _alat = _alat + 1;
-        }
-      } else {
-        print("haha");
-      }
+      // if (_buyers[i].cart != null || buyers[i].cart != []) {
+      //   buyers[i].cart = cart;
+      //   if (cart[i].productCategory == "Bibit") {
+      //     _bibit = _bibit + 1;
+      //   } else if (cart[i].productCategory == "Pupuk") {
+      //     _pupuk = _pupuk + 1;
+      //   } else if (cart[i].productCategory == "Obat") {
+      //     _obat = _obat + 1;
+      //   } else if (cart[i].productCategory == "Alat") {
+      //     _alat = _alat + 1;
+      //   }
+      // } else {
+      //   print("haha");
+      // }
     }
   }
 
